1927f6811465304c16f6992e76038a880188f776
SebasGarciaSM <75601981+SebasGarciaSM@users.noreply.github.com>
2025-07-22 13:41:37 -0400

    Remove import

--- lib/ui/widgets/error_lottie.dart
+++ lib/ui/widgets/error_lottie.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:nasa_app/l10n/app_localizations.dart';
-import 'package:nasa_app/ui/theme/app_colors.dart';
 import 'package:nasa_app/ui/theme/lottie_assets.dart';
 
 class ErrorLottie extends StatelessWidget {
